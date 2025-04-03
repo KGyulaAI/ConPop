7b8d0511301314c7b4e2077b2c59752d1b8fc04e
kovacs.gyula <kovacs.gyula@BURG-1E05-12>
2025-04-03 08:29:56 +0200

    Add project files.

---
+++ City.cs
@@ -0,0 +1,32 @@
+﻿namespace ConPop
+{
+    public class City
+    {
+        string cityCode;
+        string cityName;
+        int y2010;
+        int y2020;
+        int y2030;
+        int y2040;
+        int y2050;
+
+        public City(string cityCode, string cityName, int y2010, int y2020, int y2030, int y2040, int y2050)
+        {
+            this.cityCode = cityCode;
+            this.cityName = cityName;
+            this.y2010 = y2010;
+            this.y2020 = y2020;
+            this.y2030 = y2030;
+            this.y2040 = y2040;
+            this.y2050 = y2050;
+        }
+
+        public string CityCode { get => cityCode; }
+        public string CityName { get => cityName; }
+        public int Y2010 { get => y2010; }
+        public int Y2020 { get => y2020; }
+        public int Y2030 { get => y2030; }
+        public int Y2040 { get => y2040; }
+        public int Y2050 { get => y2050; }
+    }
+}
---
+++ CityDataProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConPop
+{
+    class CityDataProvider : ICityMethods
+    {
+        List<City> cities = new List<City>();
+
+        public List<City> Cities { get => cities; }
+
+        public int CityCount()
+        {
+            return cities.Count;
+        }
+
+        public bool IsContinuousGrowing(List<int> populationDatas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadFromCSV(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(';');
+                cities.Add(new City(line[0], line[1], Convert.ToInt32(line[2]), Convert.ToInt32(line[3]), Convert.ToInt32(line[4]), Convert.ToInt32(line[5]), Convert.ToInt32(line[6])));
+            }
+            sr.Close();
+        }
+
+        public void SaveToCSV(string path, List<City> cities, string charCode = "UTF-8")
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<City> Top10City(int year)
+        {
+            IOrderedEnumerable<City> filteredCities = Enumerable.Empty<City>().OrderBy(x => 0);
+            switch (year)
+            {
+                case 2010:
+                    filteredCities = cities.OrderByDescending(x => x.Y2010);
+                    break;
+                case 2020:
+                    filteredCities = cities.OrderByDescending(x => x.Y2020);
+                    break;
+                case 2030:
+                    filteredCities = cities.OrderByDescending(x => x.Y2030);
+                    break;
+                case 2040:
+                    filteredCities = cities.OrderByDescending(x => x.Y2040);
+                    break;
+                case 2050:
+                    filteredCities = cities.OrderByDescending(x => x.Y2050);
+                    break;
+                default:
+                    break;
+            }
+            return filteredCities.Take(10).ToList();
+        }
+    }
+}
